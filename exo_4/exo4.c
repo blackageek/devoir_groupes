27468c97278ed4ef42b99695e79a692f1d8d7131
lafia tikande <90789460+blackageek@users.noreply.github.com>
2022-03-15 19:45:45 +0000

    Update exo4.c

--- exo_4/exo4.c
+++ exo_4/exo4.c
@@ -1,21 +1,26 @@
 #include <sys/types.h>
+
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
 #include "md5.h"
 
 
 
 
 
+
 int main(int argc , char *argv[]){
-char data[512];
+char buf[512];
 if(argc != 2) {
-fprintf(stderr, "Syntaxe: %s fichier\n", argv[0]);
+fprintf(stderr, "Syntaxe: ./%s fichier_a_calculer\n", argv[0]);
 exit(1);
 }
-unsigned char *md5=NULL;
-md5= MD5File(argv[1], data);
-printf(" le code md5 est %s %s", md5, argv[1]);
+unsigned char *MD5=NULL;
+MD5= MD5File(argv[1], buf);
+fprintf(stderr," Code MD5 : %s %s", MD5, argv[1]);
 return 0;
 }
