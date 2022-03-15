dfd36896e812279ed14e96750b479958b536974a
jacqueboys3 <101189067+jacqueboys3@users.noreply.github.com>
2022-03-15 20:17:13 +0100

    Update exo5.c

--- exo_5/exo5.c
+++ exo_5/exo5.c
@@ -8,29 +8,8 @@
 #include <errno.h>
 #include "md5.h"
 
-
-int main(int argc, char **argv)
 {
-int fd1, fd2, rc;
-char buf;
-char buf1[512];
-char buf2[512];
-unsigned char *MD5_1=NULL;
-unsigned char *MD5_2=NULL;
-if(argc != 3) {
-fprintf(stderr, "Syntaxe: %s fichier_source fichier_destination\n", argv[0]);
-exit(1);
-}
-
-   fd1 = open(argv[1], O_RDONLY);
-   if(fd1 < 0) {
-   perror("fichier(fc1)");
-   exit(1);
-}
-
-fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-   if(fd2 < 0) {
-   perror("fichier(fc2)");
+   erreur ( "fichier(fc2)" );
    exit(1);
 }
 
@@ -59,7 +38,7 @@ exit(1);
 close(fd1);
 close(fd2);
 MD5_1= MD5File(argv[1], buf1);
-MD5_2= MD5File(argv[2], buf2);
+MD5_2 =  MD5File ( argv [ 2 ], buf2 );
 
 
 if (strcmp(MD5_1, MD5_2)==0) {
