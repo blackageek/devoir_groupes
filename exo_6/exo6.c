86095e673161017a3718aa6dc8ef46e85a0aee9f
GUIDIME Josephine <101390636+Teuvi@users.noreply.github.com>
2022-03-15 19:57:16 +0000

    Update exo6.c

--- exo_6/exo6.c
+++ exo_6/exo6.c
@@ -11,29 +11,29 @@
 
 
 int main(int argc , char *argv[]){
-char data[512];
+char buf[512];
 int i=1;
 char file[1000];
-FILE *resultat=NULL;
-resultat=fopen("MD5_1000.txt","w+");
+FILE *fich=NULL;
+fich=fopen("MD5_1000.txt","w+");
 unsigned char *MD5=NULL;
 
 while (i <1000{
-    printf("saisie le nom du fichier (%d) :",i+1);
+    printf("Saisie le nom du fichier (%d) \n :",i+1);
     scanf("%s",file);
-    MD5= MD5File(file, data);
+    MD5= MD5File(file, buf);
     if (MD5==NULL){
-    printf("Impossible de trouver fichier (%s) \n", file);
+    printf("Impossible de trouver le fichier (%s) \n", file);
     i=i-1;
     }
     else {
-     fprintf(resultat, "%s %s \n", MD5, file);
+     fprintf(fich, "%s %s \n", MD5, file);
     }
     
    
     i=i+1;
 }
-fclose(resultat);
+fclose(fich);
 
 return 0;
 }
